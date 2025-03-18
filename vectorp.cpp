15a6d83afaf54c18fddf4d10ed34aaca962eef50
vishalalewar <alewarv@gmail.com>
2025-03-18 17:03:22 +0530

    vector print

---
+++ vectorp.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int>  arr;
+	int n;
+	cout<<"Enter Num:";
+	cin>>n;
+	cout<<"Enter vector:";
+       for(int i=0;i<n;i++)
+       {
+           cin>>arr.push()
+       
+       }
+
+       for(int j=0;j<arr.size();j++)
+       {
+	       cout<<arr[j];
+       }
+       cout<<"\n";
+
+ } 
